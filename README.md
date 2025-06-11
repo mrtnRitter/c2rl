1fb5d40a14d1622b902052fcbee23efbe09ec3e3
mrtnRitter <mrtn.ritter@googlemail.com>
2025-06-11 19:28:49 +0200

    init

---
+++ README.md
@@ -0,0 +1,67 @@
+## Codetwo License Reset (c2rl)
+
+Dieses Skript automatisiert die Überwachung und das Zurücksetzen von Lizenzen im Codetwo Email Signatures 365 Dashboard.
+
+<br>
+
+>- Download aktuellen Release an beliebigen Speicherort
+>- c2rl.exe starten
+
+<br>
+
+Es läuft **nur unter Windows** als autostart Tray-Anwendung mit folgenden Hauptfunktionen:
+
+- **Automatisches Lizenz-Monitoring:**  
+  Überwacht kontinuierlich die aktuelle Lizenznutzung im Hintergrund.
+
+- **Automatischer Lizenz-Reset:**  
+  Setzt Lizenzzähler so bald wie möglich automatisch zurück.
+  
+- **Reset-Lock-Countdown:**  
+  Zeigt im Tray-Menü an, wann der nächste Lizenz-Reset möglich ist.
+
+- **Auto-Login & manuelles Login:**  
+  Versucht automatisch, sich im Codetwo-Dashboard anzumelden. Falls dies fehlschlägt, wird ein manueller Login im Browser ermöglicht.
+
+- **Statusanzeige im System-Tray:**  
+  Das Tray-Icon signalisiert, wenn nur noch 2 Lizensen verfügbar sind.
+  
+- **Konfigurierbar:**  
+  Einstellungen wie Ziel-URL, Zeitabstände und Debug-Modus werden über eine config.ini verwaltet.
+
+- **Logging:**  
+  Alle Aktionen und Fehler werden in einer rotierenden Logdatei protokolliert.
+
+- **Einfache Bedienung:**  
+  Über das Tray-Menü können Einstellungen, Infos und das Beenden der App aufgerufen werden.
+
+---
+
+Die Datei config.ini steuert das Verhalten des Skripts. Sie enthält folgende Einstellungen:
+
+| Name                | Beschreibung                                                                                   |
+|---------------------|----------------------------------------------------------------------------------------------- |
+| `target_url`        | Die URL zum Codetwo-Lizenz-Dashboard, das überwacht und automatisiert werden soll.             |
+| `user_data_dir`     | Pfad zum Chrome-Benutzerprofil-Ordner - wird automatisch gesetzt.                              |
+| `profile_dir`       | Name des Chrome-Profils bestehenden Login - wird automatisch gesetzt.                          |
+| `discover_timeout`  | Zeit in Sekunden, wie lange auf das Laden von Elementen im Browser gewartet wird.              |
+| `watchdog_timeout`  | Intervall in Sekunden, wie oft die Lizenzüberwachung ausgeführt wird.                          |
+| `debug`             | `True` für Fenstermodus, `False` für fensterlosen Hintergrundmodus                             |
+
+> **Hinweis:**  
+> config.ini wird beim ersten Start automatisch erstellt und kann nachträglich angepasst werden.  
+> Pfad- und Profilangaben werden automatisch ermittelt.
+
+---
+
+Für die Ausführung als Skript werden der **Chrome Browser** und diese Module benötigt: 
+- **selenium**
+- **pystray**
+- **pillow (PIL)**
+- **pywin32**
+
+Installation:
+```
+pip install selenium pystray pillow win32com.client
+```
+
---
+++ script/c2rl.py
@@ -0,0 +1,517 @@
+# ------------- IMPORTS -------------
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException, WebDriverException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from datetime import datetime, timedelta
+from zoneinfo import ZoneInfo
+import configparser
+import time
+import sys
+import re
+import os
+import threading
+import pystray
+from pystray import MenuItem as item
+from PIL import Image
+import logging
+from logging.handlers import RotatingFileHandler
+import math
+import win32com.client
+
+
+
+# ------------- GLOBALS -------------
+app_name = "c2rl"
+app_description = "Codetwo License Reset"
+app_version = "v1.0"
+app_author = "https://github.com/mrtnRitter"
+
+driver = None
+target_url = None
+user_data_dir = None
+profile_dir = None
+discover_timeout = None
+watchdog_timeout = None
+debug = None
+
+base_path = None
+idle_str = "Warte auf Daten ..."
+timeout_seconds = 0
+menu_timeout_str = idle_str
+menu_license_str = idle_str
+app_status = "default"
+
+
+
+# ------------- RESOURCE PATH -------------
+def resource_path(relative_path):
+    """ 
+    Get the absolute path to a resource, works for both frozen and non-frozen applications.
+    """
+    if hasattr(sys, '_MEIPASS'):
+        return os.path.join(sys._MEIPASS, relative_path)
+    return os.path.join(os.path.abspath("."), relative_path)
+
+
+
+# ------------- FUNCTIONS -------------
+def init():
+    """
+    Setup and configure the script.
+    """
+    global target_url
+    global user_data_dir
+    global profile_dir
+    global discover_timeout
+    global watchdog_timeout
+    global debug
+    global base_path
+
+    if getattr(sys, 'frozen', False):
+        base_path = os.path.dirname(sys.executable)
+        check_autostart_lnk()
+    else:
+        base_path = os.path.dirname(os.path.abspath(__file__))
+
+    setup_logging()
+    logging.info(f"Starting {app_name} {app_version}")
+
+    config_path = os.path.join(base_path, "config.ini")
+    if not os.path.exists(config_path):
+        make_config(config_path)
+        exit()
+
+    cfg = parse_config(config_path)
+    if not cfg:
+        make_config(config_path)
+        cfg = parse_config(config_path)
+        if not cfg:
+            logging.critical("Config file could not be read after re-creation - App quit!")
+            exit()
+    
+    target_url, user_data_dir, profile_dir, discover_timeout, watchdog_timeout, debug = cfg
+
+    if not target_url:
+        logging.error("Tenant ID is missing in the config file - App quit!")
+        exit()
+
+    if not user_data_dir or not profile_dir:
+        add_browser_profile_to_config(config_path)
+
+
+def check_autostart_lnk():
+    """
+    Create autostart entry for EXE if needed.
+    """
+    autostart = os.path.join(os.getenv('APPDATA'), r"Microsoft\Windows\Start Menu\Programs\Startup")
+    exe_path = os.path.join(base_path, f"{app_name}.exe")
+    shortcut = os.path.join(autostart, f"{app_name}.lnk")
+
+    if not os.path.exists(shortcut):
+        shell = win32com.client.Dispatch("WScript.Shell")
+        shortcut_obj = shell.CreateShortCut(shortcut)
+        shortcut_obj.Targetpath = exe_path
+        shortcut_obj.WorkingDirectory = base_path
+        shortcut_obj.save()
+
+
+
+def setup_logging():
+    """
+    Setup logging configuration.
+    """
+    log_path = os.path.join(base_path, "c2rl_log.log")
+    logging.basicConfig(
+        level=logging.INFO,
+        format="[%(levelname)s] %(asctime)s %(threadName)s %(funcName)s: %(message)s",
+        handlers=[RotatingFileHandler(
+            log_path,
+            maxBytes=10*1024*1024,
+            backupCount=5,
+            encoding="utf-8"
+            )]
+    )
+
+
+
+def add_browser_profile_to_config(config_path):
+    """
+    Add the browser profile path to the config file.
+    """
+    logging.info("No browser profile in config file, attempting to add it.")
+    
+    global user_data_dir
+    global profile_dir
+    global driver
+    
+    setup_driver(headless=True)
+    driver.get("chrome://version")
+    profile_path = driver.find_element(By.ID, "profile_path").text
+    driver.quit()
+    driver = None
+
+    user_data_dir = os.path.dirname(os.path.dirname(profile_path))
+    profile_dir = os.path.basename(os.path.dirname(profile_path))
+
+    config = configparser.ConfigParser()
+    config.read(config_path)
+    config["DEFAULT"]["user_data_dir"] = user_data_dir
+    config["DEFAULT"]["profile_dir"] = profile_dir
+    with open(config_path, "w") as f:
+        config.write(f)
+
+
+
+def make_config(config_path):
+    """
+    Create a default config file.
+    """
+    logging.info("No config file found, creating from scratch.")
+    logging.info("Add tenant ID in config file to use the app.")
+
+    with open(config_path, "w") as f:
+        f.write(f"# Config file for {app_name} {app_version}\n")
+        f.write("[DEFAULT]\n")
+        f.write("tenant =\n")
+        f.write("user_data_dir =\n")
+        f.write("profile_dir =\n")
+        f.write("discover_timeout = 10\n")
+        f.write("watchdog_timeout = 600\n")
+        f.write("debug = False\n")
+
+
+
+def parse_config(config_path):
+    """
+    Parse the config file.
+    """
+    logging.info("Reading config file...")
+
+    config = configparser.ConfigParser()
+    config.read(config_path)
+
+    try:
+        tenant = config.get("DEFAULT", "tenant")
+        if tenant:
+            target_url = f"https://emailsignatures365.codetwo.com/dashboard/tenants/{tenant}/licenses"
+        else:
+            target_url = None
+        user_data_dir = config.get("DEFAULT", "user_data_dir")
+        profile_dir = config.get("DEFAULT", "profile_dir")
+        discover_timeout = config.getint("DEFAULT", "discover_timeout")
+        watchdog_timeout = config.getint("DEFAULT", "watchdog_timeout")
+        debug = config.getboolean("DEFAULT", "debug")
+        return target_url, user_data_dir, profile_dir, discover_timeout, watchdog_timeout, debug
+
+    except Exception as e:
+        logging.error(f"Error reading config file: {e}")
+        return False
+
+
+
+def setup_driver(headless):
+    """
+    Setup the Chrome driver with options.
+    """
+    logging.info("Setting up browser...")
+    
+    global driver
+    options = Options()
+
+    if debug:
+        headless = False
+        options.add_experimental_option("detach", True)
+    else:
+        headless = headless
+    
+    if headless:
+        options.add_argument("--headless")
+    
+    options.add_argument("--log-level=3")
+    options.add_argument("--ignore-certificate-errors")
+    options.add_argument("--ignore-ssl-errors")
+    options.add_argument("--window-size=800,1000")
+
+    if user_data_dir and profile_dir:
+        options.add_argument(f"user-data-dir={user_data_dir}")
+        options.add_argument(f"profile-directory={profile_dir}")
+
+    driver = webdriver.Chrome(options=options)
+    driver.implicitly_wait(discover_timeout)
+
+    driver.get(target_url)
+    time.sleep(discover_timeout)
+    logging.info("Browser ready.")
+
+
+
+def auto_login():
+    """
+    Fullfil login procedure.
+    """
+    logging.info("Attempting to auto login...")
+    try:
+        driver.find_element(By.ID, "AzureADCommonSigninExchange").click()
+        driver.find_element(By.XPATH, "//small[text()='Angemeldet']").click()
+        logging.info("Auto login successful.")
+        return True
+        
+    except Exception as e:
+        logging.error("Auto login failed: " + str(e).splitlines()[0])
+        return False
+
+
+
+def manual_login():
+    """
+    Open the browser for manual login and session renewal.
+    """
+    global driver
+    if driver:
+        driver.quit()
+        driver = None
+    
+    setup_driver(headless=False)
+    logging.info("Open Browser and waiting for user to manual login...")
+
+    while True:
+        try:
+            driver.title
+            time.sleep(discover_timeout)
+        except WebDriverException:
+            driver = None
+            break
+
+
+
+def get_menu_license_str():
+    """
+    Get the license count from the page.
+    """
+    logging.info("Attempting to fetch license count from page...")
+
+    global menu_license_str
+    lic_usage = "N/A"
+    
+    try:
+        lic_usage = driver.find_element(By.TAG_NAME, "dd").text
+        menu_license_str = f"Lizenzen: {lic_usage}"
+        logging.info(f"{menu_license_str} currenently in use.")
+        return True
+
+    except NoSuchElementException as e:
+        logging.error("License counter not found: " + str(e).splitlines()[0])
+        return False
+
+
+
+def reset_license_counter():
+    """
+    Reset the license count.
+    """
+    logging.info("Attempting to click license reset button...")
+    
+    try:
+        driver.refresh()
+        btns = driver.find_elements(By.CSS_SELECTOR, "button.c2-button.c2-button--solid")
+        for btn in btns:
+            if btn.find_element(By.TAG_NAME, "span").get_attribute("outerHTML") == '<span class="display-contents">Reset license count (signature)</span>':
+                btn.click()
+                time.sleep(5)
+                logging.info("License reset button clicked.")
+                return True
+            
+    except Exception as e:
+        logging.error("License reset failed: " + str(e).splitlines()[0])
+        return False
+    
+    logging.warning(f"License reset button not found on page.")
+    return False
+
+
+
+def get_timeout_seconds():
+    """
+    Get last reset date and return timeout to next reset. Always run reset_license_counter() first.
+    """    
+    logging.info("Attempting to fetch reset lock timeout...")
+    
+    global timeout_seconds
+    
+    try:
+        msg = driver.find_element(By.CLASS_NAME, "c2-message-text").text
+        match = re.search(r"on ([A-Za-z]+\s\d{1,2},\s\d{4}) at ([\d:]+\s[AP]M\sUTC)", msg)
+        if match:
+                date_str = match.group(1)
+                time_str = match.group(2)
+
+                dt_str = f"{date_str} {time_str.replace(' UTC', '')}"
+                utc_dt = datetime.strptime(dt_str, "%B %d, %Y %I:%M %p")
+                utc_dt = utc_dt.replace(tzinfo=ZoneInfo("UTC"))
+                berlin_dt = utc_dt.astimezone(ZoneInfo("Europe/Berlin"))
+
+                last_reset_date = berlin_dt.strftime("%d.%m.%Y")
+                last_reset_time = berlin_dt.strftime("%H:%M")
+                timeout_seconds = int(math.ceil(((berlin_dt + timedelta(hours=24)) - datetime.now(ZoneInfo("Europe/Berlin"))).total_seconds())) + 60
+                driver.find_element(By.CLASS_NAME, "c2-dlg-button ").click()
+
+                logging.info(f"Last reset was: {last_reset_date}, at: {last_reset_time}, reset locked for: {timeout_seconds} seconds.")
+                time.sleep(5)
+                return True
+
+        else:
+                logging.error(f"Could not find any dates in: '{msg}'")
+                return False    
+
+    except NoSuchElementException as e:
+        logging.error("Reset lock timeout not found: " + str(e).splitlines()[0])
+        return False
+
+
+
+def update_reset_lock_timeout(app):
+    """
+    Set the reset lock timeout in the UI.
+    """
+    logging.info("Updating reset lock timeout in UI...")
+    
+    global timeout_seconds
+    global menu_timeout_str
+    
+    while timeout_seconds > 0:
+        h, m = timeout_seconds // 3600, (timeout_seconds % 3600) // 60
+
+        if m >= 30:
+            h += 1
+
+        if timeout_seconds >= 3600:
+            menu_timeout_str = f"Reset in {int(h)}h"
+            update_interval = 3600
+        elif timeout_seconds >= 600:
+            menu_timeout_str = f"Reset in {int(m)}m"
+            update_interval = 600
+        elif timeout_seconds >= 60:
+            menu_timeout_str = f"Reset in {int(m)}m"
+            update_interval = 60
+
+        app.menu = build_menu()
+
+        sleep_time = min(update_interval, timeout_seconds)  
+        logging.info(f"Reset locked for: {timeout_seconds} seconds, next update in {sleep_time} seconds.")
+
+        time.sleep(sleep_time)
+        timeout_seconds -= sleep_time
+
+        
+
+def timeout_and_reset(app):
+    """
+    Handles license reset, reset lock timeout and updates the UI.
+    """
+    while True:
+        if not driver:
+            setup_driver(headless=True)
+
+        if reset_license_counter():
+            if get_timeout_seconds():
+                update_reset_lock_timeout(app)
+
+        elif auto_login():
+            time.sleep(discover_timeout)
+            pass
+                
+        else:
+            manual_login()
+
+        time.sleep(discover_timeout)
+
+        
+
+def license_watchdog(app):
+    """
+    Watchdog thread to monitor the license counter.
+    """
+    global app_status
+
+    while True:
+        while timeout_seconds > 60:
+            if not driver:
+                setup_driver(headless=True)
+
+            if get_menu_license_str():
+                app.menu = build_menu()
+
+                in_use, total = map(int, menu_license_str.split(":")[1].strip().split("/"))
+                if in_use >= total-1 and app_status == "default":
+                    set_tray_icon(app, "error")
+                    app_status = "error"
+                    logging.warning(f"Licenses exceeded: {menu_license_str} - auto reset might not be working!")
+
+                elif in_use < total-1 and app_status == "error":
+                    set_tray_icon(app, "default")
+                    app_status = "default"
+
+                time.sleep(watchdog_timeout)
+
+            elif auto_login():
+                time.sleep(discover_timeout)
+           
+            else:
+                manual_login()
+        
+        time.sleep(discover_timeout)
+
+
+
+# -------- UI FUNCTIONS -------------
+
+def set_tray_icon(app, status):
+    if status == "default":
+        app.icon = Image.open(ico_default)
+    elif status == "error":
+        app.icon = Image.open(ico_error)
+
+def get_settings():
+    if base_path:
+        os.startfile(base_path)
+
+def get_about():
+    os.startfile("https://github.com/mrtnRitter/c2rl")
+
+def on_quit(app):
+    global driver
+    if driver:
+        driver.quit()
+        driver = None
+    logging.info("App closed by user.")
+    app.stop()
+
+def build_menu():
+    return pystray.Menu(
+        item(f"{menu_license_str}", None),
+        item(f"{menu_timeout_str}", None),
+        pystray.Menu.SEPARATOR,
+        item("Einstellungen", get_settings),
+        item("Über", get_about),
+        item("Beenden", on_quit)
+    )
+
+
+
+# ------------- MAIN -------------
+if __name__ == "__main__":
+
+    ico_default = resource_path("script/res/ico_default.ico")
+    ico_error = resource_path("script/res/ico_error.ico")
+
+    init()
+    app = pystray.Icon(app_description, Image.open(ico_default), app_description + " " + app_version, build_menu())
+
+    timeout_and_reset_t = threading.Thread(target=timeout_and_reset, args=(app,), daemon=True)
+    timeout_and_reset_t.start()
+
+    license_watchdog_t = threading.Thread(target=license_watchdog, args=(app,), daemon=True)
+    license_watchdog_t.start()
+
+    app.run()
